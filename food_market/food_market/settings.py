c48f5a15539a02b976ee192964503c14daf7cb24
arshak <Arshak-Yesayan>
2020-08-15 14:32:02 +0400

    HTML

--- food_market/food_market/settings.py
+++ food_market/food_market/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR,]
 
 # Media files
 
