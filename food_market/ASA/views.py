41049a7f48d916eb66d33868188fa5781929ac5a
arshak <Arshak-Yesayan>
2020-09-03 12:09:54 +0400

    Home page

--- food_market/ASA/views.py
+++ food_market/ASA/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from product.models import Category, Subcategory
 
 
 
 def index(request):
-
-    context_dict = {}
-    return render(request, 'ASA/home.html', )
+    array = []
+    categories = Category.objects.all()
+    for category in categories:
+        subcategories = Subcategory.objects.filter(category=category)
+        cat = [category, [subcategories]]
+        array.append(cat)
+    context = {'categories': array}
+    return render(request, 'ASA/home.html', context=context)
 
