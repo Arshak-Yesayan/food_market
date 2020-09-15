fd40017b2e7d25bcf1fc0140ff1483e4372622aa
Arshak Esayan <arshak.esayan@basic.local>
2020-09-15 20:38:33 +0400

    user_accounts

--- food_market/user_accounts/views.py
+++ food_market/user_accounts/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from random import choice
 import string
-from user_accounts.forms import ProfileForm, UserUpdateForm, try_register
+from user_accounts.forms import ProfileForm, UserUpdateForm
 
 
 
@@ -27,6 +27,30 @@ def update_verification():
             user_account = User.objects.get(username=user_verification.username)
             user_account.delete()
 
+def try_register(username, password1, password2, email, token, date):
+    if not (username == '' or password1 == '' or password2 == '' or email == ''):
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if password1 == password2:
+                    if len(password1) >= 8:
+                        user = User(username=username, email=email)
+                        user.set_password(password1)
+                        user.is_active = False
+                        verify = Verification(username=user, date=date)
+                        verify.token = make_password(token)
+
+                        return True, user, verify
+                    else:
+                        message = 'Passwords length is to less.'
+                else:
+                    message = 'Passwords must be same.'
+            else:
+                message = 'Email already exists.'
+        else:
+            message = 'Username already exists.'
+    else:
+        message = 'Write something to the fields.'
+    return False, message
 
 # Create your views here.
 
@@ -72,7 +96,7 @@ def verify(request):
             context['verified'] = True
     except:
         pass
-    return render(requests, 'accounts/verify.html', context=context)
+    return render(request, 'accounts/verify.html', context=context)
 
 
 def auth_login(request):
